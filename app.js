2b60f9ea74793fe422bae8169ebf0038689a678d
Adarsh7580 <adarshsingh9681@gmail.com>
2026-01-28 00:15:52 +0530

    commit

--- app.js
+++ app.js
@@ -16,9 +16,11 @@ const ejsMate = require("ejs-mate");
 
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo').default;
+
 const flash = require("connect-flash");
 const passport = require("passport");
-const LocalStratergy = require("passport-local");
+const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
 
@@ -28,7 +30,11 @@ const userRouter =require("./routes/user.js");
 
 
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/YourNEXTStop";
+//const MONGO_URL = "mongodb://127.0.0.1:27017/YourNEXTStop";
+
+const dbUrl = process.env.ATLASDB_URL;
+
+
 
 main().then(() => {
     console.log("connected to DB");
@@ -40,7 +46,7 @@ main().then(() => {
 
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
@@ -51,8 +57,35 @@ app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
 
+//const store = MongoStore.createKrupteinAdapter({
+  //  mongoUrl: dbUrl,
+    //crypto: {
+      //  secret: "mysupersecretcode",
+    //},
+    //touchAfter: 24 * 3600,
+//});
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+});
+
+
+
+
+
+
+store.on("error", () => {
+    console.log("ERROR in MONGO SESSION STORE", err);
+})
+
+
 const sessionOptions = {
-    secret: "mysupersecretcode",
+    store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -66,12 +99,14 @@ const sessionOptions = {
   //  res.send("Hi, I am root");
 //});
 
+
+
 app.use(session(sessionOptions));
 app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStratergy(User.authenticate()));
+passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
